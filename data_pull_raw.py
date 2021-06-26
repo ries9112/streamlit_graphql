727692253e2d7d6cc93d8e3ef5afb83fd6dc6a31
ries9112 <ries9112@colorado.edu>
2021-06-26 12:20:01 -0600

    test streamlit + graphql

--- data_pull_raw.py
+++ data_pull_raw.py
@@ -2,16 +2,6 @@ import requests
 import json
 import pandas as pd
 
-# Add title
-st.title('Pulling data from GraphQL')
-
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
-)
-
-# Choose query to run
 query = """query {
   account(id: "0x00000000af5a61acaf76190794e3fdf1289288a1") {
     id
@@ -28,19 +18,23 @@ query = """query {
 }
 """
 
-# Point to correct subgraph URL
+
 url = 'https://api.thegraph.com/subgraphs/name/graphprotocol/compound-v2'
-# Make the request
 r = requests.post(url, json={'query': query})
 #print(r.status_code)
 #print(r.text)
 
-# JSON adjustment
 json_data = json.loads(r.text)
 
-# extract JSON to convert to a dataframe
+
+#json_data
+
+
 df_data = json_data['data']['account']['tokens']
-# convert to dataframe
+
+#print(df_data)
+
+
 df = pd.DataFrame(df_data)    
 
-
+print(df)
