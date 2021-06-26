6edccd92b03aa6184b5cd75962e79de49234415c
ries9112 <ries9112@colorado.edu>
2021-06-26 12:47:58 -0600

    show DF

--- graphql_pull.py
+++ graphql_pull.py
@@ -7,10 +7,7 @@ import pandas as pd
 st.title('Pulling data from GraphQL')
 
 # Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
-)
+user_input = st.sidebar.text_input("Enter ETH wallet to look up", "0x00000000af5a61acaf76190794e3fdf1289288a1")
 
 # Choose query to run
 query = """query {
@@ -44,4 +41,6 @@ df_data = json_data['data']['account']['tokens']
 # convert to dataframe
 df = pd.DataFrame(df_data)    
 
+# Show dataframe
+st.write(df)
 
